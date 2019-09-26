366426a1c3d342eb398b0550eea1a4243d7469a0
Brian de Heus <me@brian.jp>
2019-09-26 11:46:04 +0900

    Create django_reset_migrations.sh

---
+++ various/django_reset_migrations.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+
+python manage.py makemigrations
+python manage.py migrate
